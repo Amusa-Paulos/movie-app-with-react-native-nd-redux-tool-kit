c6fdcd09443f4389d3db2fa59e721371e1c46c00
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-10-23 19:49:25 +0100

    The repository the movie app made with react native, typescript  and redux toolkit

---
+++ components/shared/constants.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import React from "react";
+
+const BASE_ENDPOINT_URL = 'https://api.themoviedb.org/3/'
+export const DETAILS_ENDPOINT = "movie/"
+export const LATEST_MOVIES_ENDPOINT = "trending/movie/day?language=en-US&page="
+export const SEARCH_MOVIES_ENDPOINT = "search/movie?language=en-US&query="
+
+export const axios_instance = axios.create({
+    baseURL: BASE_ENDPOINT_URL,
+    timeout: 10000,
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer <Get your own bearer token from TMDB here: https://developer.themoviedb.org/v3/reference/intro/authentication#api-key-quick-start>'
+    }
+});
+
+export const IMAGE_URL = "https://image.tmdb.org/t/p/original"
+
+export const HomeTabContext = React.createContext({})
